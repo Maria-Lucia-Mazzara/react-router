6483237ca4231675e249e927a46fa7ef78820859
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2025-11-15 18:43:00 +0100

    aggiunto elemento al componente HomePage

--- src/assets/Components/HomePage.jsx
+++ src/assets/Components/HomePage.jsx
@@ -5,7 +5,9 @@ function HomePage() {
 
     return (
         <>
-
+            <div className="container">
+                <h1 className="text-center mt-3">Benvenuto nel nostro Mini E-Commerce!</h1>
+            </div>
         </>
     )
 }
